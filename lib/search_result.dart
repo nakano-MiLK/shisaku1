0277dbb703847e17ea9c972171548306bb6ab5df
nakano-MiLK <ken4194114@gmail.com>
2022-06-02 08:57:52 +0900

    a

--- lib/search_result.dart
+++ lib/search_result.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
-class SearchResult extends StatelessWidget{
-  const SearchResult({Key? key}) : super(key: key);
+final items = List<String>.generate(10000,(i) => "Item $i");
 
-  //const SearchResult({Key? key}) : super(key: key);
+class SearchResult extends StatelessWidget{
+
+  const SearchResult({Key? key, required this.items}) : super(key: key);
+  final List<String> items;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Device Search Result'),
       ),
-      body: Container(
-        color: Colors.greenAccent,
+      body: ListView.builder(
+        itemCount : items.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(items[index]),
+          );
+        }
       ),
     );
   }
