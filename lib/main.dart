8f47faac9ee54d7afdd353a3cb6d67004ed5513d
nakano-MiLK <ken4194114@gmail.com>
2022-06-04 21:27:04 +0900

    4 buttons minor correction title text's line breaks

--- lib/main.dart
+++ lib/main.dart
@@ -40,71 +40,104 @@ class HomeButton extends StatelessWidget{
   Widget build(BuildContext context){
     final _screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-            'Ad hoc communication between mobile devices during disasters'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          centerTitle: true,
+          title: Row(
+            children:  [
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:  [
+                    Container(
+                      padding: const EdgeInsets.all(3.0),
+                      child: const Text(
+                        "Ad hoc communication between mobile devices during disasters",
+                        softWrap: true,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.clip,
+                      ),
+                    ),
+                  ],
+                )
+              )
+            ],
+          ),
+        ),
       ),
       body: Center(
         child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               SizedBox(          //device search
-                width: _screenSize.width * 0.17,
+                width: _screenSize.width * 0.23,
                 height: _screenSize.height * 0.2,
                 child: Column(
                   children: [
-                    OutlinedButton.icon(
-                      icon: const Icon(Icons.search),
-                      label: const Text(''),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => SearchResult()),
-                        );
-                      },
+                    SizedBox(
+                      height: _screenSize.height * 0.06,
+                      child: OutlinedButton.icon(
+                        icon: const Icon(Icons.search),
+                        label: const Text(''),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => SearchResult()),
+                          );
+                        },
+                      ),
                     ),
                     const Text('Device\nSearch'),
                   ],
                 ),
               ),
               SizedBox(          //new file
-                width: _screenSize.width * 0.17,
+                width: _screenSize.width * 0.23,
                 height: _screenSize.height * 0.2,
                 child: Column(
                   children: [
-                    OutlinedButton.icon(
-                      icon: const Icon(Icons.upload_file),
-                      label: const Text(''),
-                      onPressed: () {},
+                    SizedBox(
+                      height: _screenSize.height * 0.06,
+                      child: OutlinedButton.icon(
+                        icon: const Icon(Icons.upload_file),
+                        label: const Text(''),
+                        onPressed: () {},
+                      ),
                     ),
                     const Text('New File'),
                   ],
                 ),
               ),
               SizedBox(          //edit
-                width: _screenSize.width * 0.17,
+                width: _screenSize.width * 0.23,
                 height: _screenSize.height * 0.2,
                 child: Column(
                   children: [
-                    OutlinedButton.icon(
-                      icon: const Icon(Icons.edit_note),
-                      label: const Text(''),
-                      onPressed: () {},
+                    SizedBox(
+                      height: _screenSize.height * 0.06,
+                      child: OutlinedButton.icon(
+                        icon: const Icon(Icons.edit_note),
+                        label: const Text(''),
+                        onPressed: () {},
+                      ),
                     ),
                     const Text('Edit'),
                   ],
                 ),
               ),
               SizedBox(          //history
-                width: _screenSize.width * 0.17,
+                width: _screenSize.width * 0.23,
                 height: _screenSize.height * 0.2,
                 child: Column(
                   children: [
-                    OutlinedButton.icon(
-                      icon: const Icon(Icons.history),
-                      label: const Text(''),
-                      onPressed: () {},
+                    SizedBox(
+                      height: _screenSize.height * 0.06,
+                      child: OutlinedButton.icon(
+                        icon: const Icon(Icons.history),
+                        label: const Text(''),
+                        onPressed: () {},
+                      ),
                     ),
                     const Text(
                       'Transmission\nHistory',
